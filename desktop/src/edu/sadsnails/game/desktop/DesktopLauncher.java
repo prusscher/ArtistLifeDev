8f346ce7782aee933a162d955b67b907e6925f54
prusscher <prusscher@gmail.com>
2017-02-13 16:31:16 -0500

    Inital Commit
    Submitting the initiaI libGDX project for desktop

---
+++ desktop/src/edu/sadsnails/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package edu.sadsnails.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import edu.sadsnails.game.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
