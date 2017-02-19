74a52a84590001b0aef7c426739a32b1d3af6a56
prusscher <prusscher@gmail.com>
2017-02-18 21:46:36 -0500

    -Created Main Menu and Game Screen to be implemented -Added Basic FPS Counter to Game Screen for testing purposes

--- core/src/edu/sadsnails/game/MyGdxGame.java
+++ core/src/edu/sadsnails/game/MyGdxGame.java
@@ -1,33 +1,34 @@
 package edu.sadsnails.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class MyGdxGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+public class MyGdxGame extends Game {
+	public SpriteBatch batch;
+	public BitmapFont font;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		font = new BitmapFont();
+		this.setScreen(new MainMenuScreen(this));
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 1, 1, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		// Calls the Game Render method
+		// Renders the current game screen
+		super.render();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		font.dispose();
 	}
 }
