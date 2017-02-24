f674243695e1907ea2fd21518557880bde7e2327
prusscher <prusscher@gmail.com>
2017-02-24 11:21:57 -0500

    Set git to ignore Bin and Build in both Modules, Conflict Mitigation

--- core/src/edu/sadsnails/game/MainMenuScreen.java
+++ core/src/edu/sadsnails/game/MainMenuScreen.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Container;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
@@ -15,14 +16,14 @@ import com.badlogic.gdx.utils.viewport.ScreenViewport;
 public class MainMenuScreen implements Screen{
 	
 	MyGdxGame game;
-	OrthographicCamera camera;
-
-	private int count;
-
-	private double fps;
 	
 	private Skin skin;
-	private TextButton start;
+	
+	private Container<TextButton> startContainer;
+	private Container<TextButton> settingsContainer;
+	private TextButton startGame;
+	private TextButton settings;
+	
 	private Stage stage;
 	
 	public MainMenuScreen(final MyGdxGame game) {
@@ -30,36 +31,41 @@ public class MainMenuScreen implements Screen{
 		Gdx.input.setInputProcessor(stage);
 		
 		this.game = game;
-		camera = new OrthographicCamera();
-		camera.setToOrtho(false, 1280, 720);
-		fps = 0.0;
-		count = 0;
+		
 		skin = new Skin(Gdx.files.internal("uiskin.json"));
-		TextButton button = new TextButton("Start Game", skin);
-		TextButton setting = new TextButton("Setting", skin);
+		
+		// Create TextButtons
+		startGame = new TextButton("Start Game", skin);
+		settings = new TextButton("Setting", skin);
 		
 		// Listeners
-		button.addListener(new ChangeListener(){
+		startGame.addListener(new ChangeListener(){
 			@Override
-			public void changed(ChangeEvent event, Actor actor) {
-				
+			public void changed(ChangeEvent event, Actor actor) { changeView(new GameScreen(game)); }
+		});
+		
+		settings.addListener(new ChangeListener(){
+			@Override
+			public void changed(ChangeEvent event, Actor actor) { 
+				// Change to settings menu or bring up settings menu
+				// changeView(new Settings(game));
 			}
 		});
 		
-		setting.addListener(new ChangeListener(){
-			@Override
-			public void changed(ChangeEvent event, Actor actor) {
-				
-			}
-		});
+		// Create Containers to fill screen
+		startContainer = new Container<TextButton>(startGame);
+		settingsContainer = new Container<TextButton>(settings);
 		
-		// position
-		button.setBounds(Gdx.graphics.getWidth() / 2 - 50, Gdx.graphics.getHeight() / 2,150 ,150/2);
-		setting.setBounds(15, 15, 100, 50);
+		startContainer.setBounds(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		settingsContainer.setBounds(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());	
 		
-		// add actors
-		stage.addActor(button);
-		stage.addActor(setting);
+		// Proper Scene2d Containers
+		startContainer.center();
+		settingsContainer.padLeft(15).left().padBottom(15).bottom();
+		
+		// add Containers
+		stage.addActor(startContainer);
+		stage.addActor(settingsContainer);
 		
 	}
 	
@@ -73,47 +79,19 @@ public class MainMenuScreen implements Screen{
 		// Clear the screen
 		Gdx.gl.glClearColor(.1f, .1f, .1f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		
-		// Update the Ortho Camera and set the projection to the spritebatch
-		// ...not really sure what it does, we'll find out later.
-		camera.update();
-		game.batch.setProjectionMatrix(camera.combined);
-		
-		// Begin the batch
-		game.batch.begin();
-		
-		// Main Menu Text
-		game.font.draw(game.batch, "Main Menu", Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2);
-		game.font.draw(game.batch, "Press Space for Ball Bounce, Click for Main Game : ", Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2 - 50);
-		
+				
 		stage.act();
 		stage.draw();
-		
-		// Draw FPS
-		fps = (int)(1/delta);
-		game.font.draw(game.batch, "FPS :" + Double.toString(fps), 20, Gdx.graphics.getHeight()-20);
-		
-		// End the sprite batch
-		game.batch.end();
-
-		// Check to see if the screen was clicked
-		if(Gdx.input.isTouched()){
-			System.out.println("Creating GameScreen");
-			game.setScreen(new GameScreen(game));
-			this.dispose();
-		}
-		else if(Gdx.input.isKeyPressed(Keys.SPACE)){
-			System.out.println("Creating BallBounce");
-			game.setScreen(new BallBounceTest(game));
-			this.dispose();
-		}
-
+	}
+	
+	private void changeView(Screen screen) {
+		game.setScreen(screen);
+		this.dispose();
 	}
 	
 	@Override
 	public void resize(int width, int height) {
-		camera.setToOrtho(false, width, height);
-		game.batch.setProjectionMatrix(camera.combined);
+	
 	}
 
 	@Override
@@ -136,7 +114,6 @@ public class MainMenuScreen implements Screen{
 
 	@Override
 	public void dispose() {
-		// TODO Auto-generated method stub
-		
+		stage.dispose();
 	}
 }
