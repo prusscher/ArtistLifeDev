37e75c97469c10d16efaae3a784d2e9bef89dd8e
dmmusiel <dmmusiel@mtu.edu>
2017-04-16 20:14:10 -0400

    nap now naps correctly
    only one nap a day, only plays animation at the proper times, outputs error to log if you've already napped

--- core/src/edu/sadsnails/game/Actions.java
+++ core/src/edu/sadsnails/game/Actions.java
@@ -370,10 +370,16 @@ public class Actions {
 		}
 	}
 		
-	public boolean canSleep() {
+	public boolean canSleep(int type) {
 		if(state.getEnergy() == 100){
 			System.out.println("You can't sleep when you're wide awake!");
 			state.log("You can't sleep when you're wide awake!");
+			((GameScreen) game.getScreen()).getUI().updateState();
+			return false;
+		}
+		else if(state.has_napped == true && type == 1){
+			System.out.println("You shouldn't take more than one nap a day!");
+			state.log("You shouldn't take more than one nap a day!");
 			((GameScreen) game.getScreen()).getUI().updateState();
 			return false;
 		}
